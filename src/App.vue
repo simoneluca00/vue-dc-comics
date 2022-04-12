f8820e609b464acb034e2de30f8bf2e90467338b
simoneluca00 <si.zaino@libero.it>
2022-04-12 15:23:21 +0200

    Initial structure

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <header>
+      <div id="logoNav">
+      <LogoComp />
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import LogoComp from './components/LogoComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LogoComp,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  header {
+    padding: 20px 0;
+
+    #logoNav {
+      width: 80%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+
+}
+
+</style>
---
+++ src/components/LogoComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="../assets/img/dc-logo.png" alt="logo DC" id="logoHeader">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogoComp',
+
+}
+</script>
+
+<style scoped lang="scss">
+
+img#logoHeader {
+  height: 70px;
+}
+
+</style>
